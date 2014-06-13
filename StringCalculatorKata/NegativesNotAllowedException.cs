3f1bdfaf3043730402689cb30e3b2534c3092012
Clark Stuth <clarkstuth@gmail.com>
2014-06-13 18:11:13 -0500

    finished up main "Beginner" part of Kata.

--- StringCalculatorKata/NegativesNotAllowedException.cs
+++ StringCalculatorKata/NegativesNotAllowedException.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace StringCalculatorKata
 {
-    public class Class1
+    public class NegativesNotAllowedException : Exception
     {
+        public NegativesNotAllowedException(string message) : base(message)
+        {
+            
+        }
     }
 }
